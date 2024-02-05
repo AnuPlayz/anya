a6d49e69f7cd0151def80ea15a9fbe8910be92fe
Aniruddh <adubge@gmail.com>
2024-02-06 03:52:38 +0530

    Add Web3Button and Contest component, update UI layout, and fix dependencies

--- my-app/app/polls/[id]/page.tsx
+++ my-app/app/polls/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { useContract, useContractRead } from "@thirdweb-dev/react";
+import { useContract, useContractRead, Web3Button } from "@thirdweb-dev/react";
+
 import {
     Card,
     CardContent,
@@ -8,12 +9,16 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import { useParams } from "next/navigation";
+import Contest from "@/components/contest";
 
 export default function Component() {
     const { contract } = useContract("0x7194f5404B7E34E8D9A27580a1fe8d63feCFF984");
-    const id = 1;
-    const { data, isLoading } = useContractRead(contract, "getPoll", [id]);
 
+    const cid = "";
+    const id = useParams().id;
+    const { data, isLoading } = useContractRead(contract, "getPoll", [id]);
+    const { data: contestants } = useContractRead(contract, "getContestants", [id])
     if (isLoading) {
         return <div>Loading...</div>;
     }
@@ -24,22 +29,48 @@ export default function Component() {
 
     return (
         <>
-            <div className="grid md:grid-flow-col md:auto-cols-max auto-rows-auto gap-5 p-3">
-                {data.map((poll: any) => (
-                    <Card className="w-[350px]">
-                        <CardHeader className="text-center">
-                            <CardTitle>{poll.title}</CardTitle>
-                        </CardHeader>
-                        <CardContent className="text-center">
-                            <img src={poll.image} alt={poll.title} />
-                            <CardDescription >{poll.description}</CardDescription>
-                        </CardContent>
-                        <CardFooter className="flex flex-row justify-center items-center">
-                            <p>Starts at: {poll.startsAt}</p>
-                            <p>Ends at: {poll.endsAt}</p>
-                        </CardFooter>
-                    </Card>
-                ))}
+            <div className="flex flex-col items-center justify-start p-10">
+                <Card className="w-[350px]">
+                    <CardHeader className="text-center">
+                        <CardTitle>{data.title}</CardTitle>
+                    </CardHeader>
+                    <CardContent className="text-center">
+                        <img src={data.image} alt={data.title} />
+                        <CardDescription >{data.description}</CardDescription>
+                    </CardContent>
+                    <CardFooter className="flex flex-col justify-center items-center">
+                        <p>Starts at: {data.startsAt.toString()}</p>
+                        <p>Ends at: {data.endsAt.toString()}</p>
+                    </CardFooter>
+                </Card>
+                <div className="p-10">
+                    <Contest />
+                </div>
+                <h2 className="mb-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+                    Current Contestants :
+                </h2>
+                <div className="grid md:grid-flow-col md:auto-cols-max auto-rows-auto">
+                    {contestants.map((contestant: any) => (
+                        <Card key={contestant.cid} className="w-[350px]">
+                            <CardHeader className="text-center">
+                                <CardTitle>{contestant.name}</CardTitle>
+                            </CardHeader>
+                            <CardContent className="text-center">
+                                <img src={contestant.image} alt={contestant.name} />
+                                <div className="m-3">
+                                    <Web3Button
+                                        contractAddress="0x7194f5404B7E34E8D9A27580a1fe8d63feCFF984"
+                                        action={(contract) => {
+                                            contract.call("vote", [id, cid])
+                                        }}
+                                    >
+                                        vote
+                                    </Web3Button>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
             </div>
         </>
     );
