7292166a154e7372620629af64396e1dfc02c04f
Aniruddh <adubge@gmail.com>
2024-02-05 16:20:28 +0530

    Add progress bar component and update dependencies

--- my-app/app/docs/page.tsx
+++ my-app/app/docs/page.tsx
@@ -1,12 +1,17 @@
+import { ProgressBar } from "@/components/progressbar";
 import { Typography } from "@/components/typography";
 
 
 export default function Working() {
-
   return (
+    <>
+        <div className="fixed inset-x-0 top-30 h-16">
+          <ProgressBar />
+        </div>
       <div className="flex flex-col p-5 m-5 items-center justify-center">
         <Typography />
         <img src="/flow.png" alt="Proof Generation" className="w-auto h-auto" />
       </div>
-   )
+    </>
+  )
 }
