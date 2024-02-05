9f233f851186f6261d77a2fac3649aa5bafca937
Aniruddh <adubge@gmail.com>
2024-02-05 11:38:37 +0530

    Update page content for voting application

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -13,12 +13,12 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          Beautifully designed voting application <br className="hidden sm:inline" />
+          built with Next.js and Solidity.
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+          Easy to host elections and polls and get real-time dashboard for the results. <br />
+          Free. Open Source. And Ready.
         </p>
       </div>
       <div className="flex gap-4">
