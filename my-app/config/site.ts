0d0579d13d60af131b6ca0a033f5ee5874286eb0
Aniruddh <adubge@gmail.com>
2024-02-05 10:23:30 +0530

    initialize

---
+++ my-app/config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Pixel Voters",
+  description:
+    "Beautifully designed blockchain voting application built by Aniruddh.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/ani__x_",
+    github: "https://github.com/AnuPlayz/anya",
+    docs: "#",
+  },
+}
