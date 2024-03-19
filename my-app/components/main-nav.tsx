b7c77c70a0203eae0da2a50baf9194f53138c25f
Aniruddh <adubge@gmail.com>
2024-03-19 21:19:35 +0530

    a

--- my-app/components/main-nav.tsx
+++ my-app/components/main-nav.tsx
@@ -1,5 +1,8 @@
 import * as React from "react"
 import Link from "next/link"
+import { SignIn, UserButton } from "@clerk/nextjs";
+import { EnvelopeOpenIcon } from "@radix-ui/react-icons"
+import { Button } from "@/components/ui/button"
 
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
@@ -13,6 +16,7 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
+      <UserButton />
       <Link href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
         <span className="inline-block font-bold">{siteConfig.name}</span>
